b971977791b4fc0adb2d08c20c3137f47f72fced
Mragakshi Garg <mragakshigarg1008@gmail.com>
2026-05-22 15:49:47 +0530

    deleting events while having meeting linked with them

--- server/src/controllers/eventTypes.controller.js
+++ server/src/controllers/eventTypes.controller.js
@@ -74,7 +74,17 @@ exports.updateEventType = async (req, res, next) => {
 exports.deleteEventType = async (req, res, next) => {
   try {
     const id = parseInt(req.params.id, 10);
-    await prisma.eventType.delete({ where: { id } });
+
+    // Delete meetings first
+    await prisma.meeting.deleteMany({
+      where: { eventTypeId: id }
+    });
+
+    // Then delete event type
+    await prisma.eventType.delete({
+      where: { id }
+    });
+
     res.json({ success: true });
   } catch (error) {
     next(error);
