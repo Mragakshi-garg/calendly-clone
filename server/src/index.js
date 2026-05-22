5d19013cbdb77b235a37bf86525d857047d9432d
Mragakshi Garg <mragakshigarg1008@gmail.com>
2026-05-22 11:58:54 +0530

    cors

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,10 @@ const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
